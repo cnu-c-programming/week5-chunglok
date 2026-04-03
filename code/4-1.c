367527cccfb78239d38beae670ee578ad48406c3
최준화 <choijunhwa072@gmail.com>
2026-04-03 12:08:17 +0900

    Add echo function to read and print input

---
+++ code/4-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void echo() {
+    char buf[5];
+    scanf("%4s\n", buf);
+    printf("%s\n", buf);
+}
+int main() {
+    int x = 42;
+    echo();
+    return 0;
+}
