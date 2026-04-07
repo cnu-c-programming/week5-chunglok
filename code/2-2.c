b4e655c7fbb087371a3d14676d101a9f0a134007
최준화 <choijunhwa072@gmail.com>
2026-04-07 23:20:25 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -5,19 +5,21 @@ void sort_strings(char **arr, int size){
     char *temp;
     for(int i = 0; i < size-1; i++){
         for(int j = 0; j < size-1-i; j++){
-            if(strcmp(*arr[i], *arr[j])){
-                   printf(
+            if(strcmp(*arr[i], *arr[j]) != 0){
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
     }
 }
 int main() {
-    char* words[] = {"apple", "ant", "banana", "cherry");
-    int size 4;
+    char* words[] = {"apple", "ant", "banana", "cherry"};
+    int size = 4;
 
     sort_strings(words, size);
 
-    for(int i = 0; i < size = i++){
+    for(int i = 0; i < size; i++){
         printf("%s\n", words[i]);
     }
     return 0;
