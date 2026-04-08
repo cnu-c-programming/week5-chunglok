dd52a445fff8af69aa74efc234d45d31966d5732
최준화 <choijunhwa072@gmail.com>
2026-04-08 14:31:21 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -3,12 +3,12 @@
 
 void sort_strings(char **arr, int size){
     char *temp;
-    for(int i = 0; i < size-1; i++){
-        for(int j = 0; j < size-1-i; j++){
-            if(strcmp(*arr[i], *arr[j]) != 0){
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+    for(int i = 0; i < size - 1; i++){
+        for(int j = 0; j < size - 1 - i; j++){
+            if(strcmp(*arr[j + 1], *arr[j]) < 0){
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
